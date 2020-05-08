fb5a69b99a953d8531cdbd257c53107ce1f6441b
mqzhifu <mqzhifu@sina.com>
2020-05-08 12:40:46 +0800

    实例化
    实例化

--- doc/import_mysql.sh
+++ doc/import_mysql.sh
@@ -14,11 +14,11 @@ echo "$HOST:$PORT $DB $USER $PS $DIR";
 DEL_DB="drop database ${DB}"
 ADD_DB="create database ${DB} charset=utf8"
 
-mysql  -h${HOST} -u${USER} -p${MYSQL_PWD} --default-character-set=utf8  -e"${DEL_DB};${ADD_DB};"
+mysql  -h${HOST} -u${USER} --default-character-set=utf8  -e"${DEL_DB};${ADD_DB};"
 echo ${DEL_DB}
 echo ${ADD_DB}
 
-mysql  -h${HOST} -u${USER} -p${MYSQL_PWD} --default-character-set=utf8 -D${DB}  -e "source ${DIR}/structure.sql"
+mysql  -h${HOST} -u${USER} --default-character-set=utf8 -D${DB}  -e "source ${DIR}/structure.sql"
 echo "source ${DIR}/structure.sql"
 
 
@@ -26,4 +26,5 @@ echo "source ${DIR}/structure.sql"
 for file in ./init_data/*
 do
 echo $file
-mysql  -h${HOST} -u${USER} -p${MYSQL_PWD} --default-character-set=utf8 -D${DB} -e"source ${DIR}/${file}"
+mysql  -h${HOST} -u${USER} --default-character-set=utf8 -D${DB} -e"source ${DIR}/${file}"
+done
