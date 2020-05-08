3a68c86de602241c204afd334c7cae2d0c1f220c
mqzhifu <mqzhifu@sina.com>
2020-05-08 14:40:42 +0800

    ddd
    ddd

--- configcenter/instantplayadmin/env/pre/domain_cn.php
+++ configcenter/instantplayadmin/env/pre/domain_cn.php
@@ -1,5 +1,5 @@
 <?php
 //网站域名
-define('DOMAIN_URL', "admin.900day.com");
+define('DOMAIN_URL', "admin.day900.com");
 //静态文件域名
-define ('STATIC_URL', "static.900day.com");
+define ('STATIC_URL', "static.day900.com");
