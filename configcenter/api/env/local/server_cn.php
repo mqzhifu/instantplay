8d628b1cc62ebb45e17e476a9fff30b350f339b0
mqzhifu <mqzhifu@sina.com>
2020-05-08 13:00:10 +0800

    aa
    aaa

--- configcenter/api/env/local/server_cn.php
+++ configcenter/api/env/local/server_cn.php
@@ -2,16 +2,16 @@
 $arr = array(
     'ws'=>array(
         'bind_ip'=>'0.0.0.0',
-        'ip'=>'49.51.252.214',
-        'port'=>'29550',
+        'ip'=>'129.204.250.115',
+        'port'=>'19550',
         'process_name'=>'ws',
         'http_bind_ip'=>'0.0.0.0',
         'http_port'=>8088,
     ),
     'gameMatch'=>array(
         'bind_ip'=>'0.0.0.0',
-        'ip'=>'49.51.252.214',
-        'port'=>'28550',
+        'ip'=>'129.204.250.115',
+        'port'=>'18550',
         'process_name'=>'gameMatch',
         'http_bind_ip'=>'0.0.0.0',
         'http_port'=>8088,
