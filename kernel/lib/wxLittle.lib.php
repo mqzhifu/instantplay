1e8191e577b8b706a29d1cd657d4e3af9002a7c8
mqzhifu <mqzhifu@sina.com>
2020-05-20 13:24:09 +0800

    sdf
    sdfsdf

--- kernel/lib/wxLittle.lib.php
+++ kernel/lib/wxLittle.lib.php
@@ -16,7 +16,7 @@ class WxLittleLib{
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_TIMEOUT, 500);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($curl, CURLOPT_URL, $url);
         $res = curl_exec($curl);
         curl_close($curl);
