95b446425768704e0c6b8a925d36c567c121e548
mqzhifu <mqzhifu@sina.com>
2020-05-22 16:05:51 +0800

    11
    111

--- api/ctrl/wxLittleCallback.ctrl.php
+++ api/ctrl/wxLittleCallback.ctrl.php
@@ -2,6 +2,25 @@
 class WxLittleCallback{
 
     function receive(){
+        $this->checkSignature();
+    }
 
+    private function checkSignature()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if ($tmpStr == $signature ) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
